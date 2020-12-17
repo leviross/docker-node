0c64c792be0484d1eeabdfb4b7cea8d2ba2ca60d
Levi Ross <levross@gmail.com>
2020-12-17 14:40:35 +0200

    2nd commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bye there!');
+});
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
